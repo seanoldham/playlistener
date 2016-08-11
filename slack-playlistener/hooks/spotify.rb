64fc4d029dfc9a9945cc68234bf56a9dc3b9202b
Sean Oldham <seanosaur@me.com>
2016-08-11 10:04:12 -0500

    clean up time

--- slack-playlistener/hooks/spotify.rb
+++ slack-playlistener/hooks/spotify.rb
@@ -2,17 +2,19 @@ module SlackPlaylistener
   class Server < SlackRubyBot::Server
     on 'message' do |client, data|
 
-      # Authenticates Spotify App
-      def self.spotify_auth(pub, sec)
-        puts 'Authenticating...'
-        auth = RSpotify.authenticate(pub, sec)
-        puts 'Authentication complete.' if auth
-      end
+      # # Authenticates Spotify App
+      # # We don't want to auth each time a message appears, that's nuts!
+      # # Commenting out until we find a better place to do this, probably initializers
+      # def self.spotify_auth(pub, sec)
+      #   puts 'Authenticating...'
+      #   auth = RSpotify.authenticate(pub, sec)
+      #   puts 'Authentication complete.' if auth
+      # end
 
       # Find Track on Spotify
       def self.find_track(track_id, client, data)
         found_track = RSpotify::Track.find(track_id)
-        
+
         # Right now it just spits it back out to Slack.
         # Eventually, this will add the song to a given playlist
         client.say(channel: data.channel, text: found_track.name)
@@ -21,17 +23,16 @@ module SlackPlaylistener
         puts 'Invalid track ID. Please try again.'
       end
 
-      spotify_auth(ENV['SPOTIFY_CLIENT'], ENV['SPOTIFY_SECRET'])
-
       # The Slack data, parsed to remove everything but the track ID
       if /track\//.match(data.text)
-        id = $'
-        id = id[0..-2]
-        find_track(id, client, data)
+        # $' does this cool thing where it grabs everything behind the match.
+        find_track($'[0..-2], client, data)
       else
         # Change this to 'stop process' or whatever the Ruby thing for that is
-        puts "No match..."
+        puts 'No match...'
       end
+
+      # spotify_auth(ENV['SPOTIFY_CLIENT'], ENV['SPOTIFY_SECRET'])
 
       # Do playlist stuff here
 
